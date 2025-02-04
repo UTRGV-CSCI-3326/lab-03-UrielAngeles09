d68514b66a0307b57b0a4ac902c370425cb42098
UrielAngeles09 <uriel.angeles01@utrgv.edu>
2025-02-04 03:29:46 +0000

    Done with lab 3

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,26 @@
+public class BankAccount {
+    public static void main(String[] arg){
+        double BobsBalance = 1275.37;
 
+        // Bob withdrew $302.50.
+        BobsBalance -= 302.50;
+
+        // Bob deposited $50.03.
+        BobsBalance += 50.03;
+
+        // Bob withdrew half of his current balance, then deposited $20.00.
+        BobsBalance -= BobsBalance / 2;
+        BobsBalance += 20.00;
+
+        // Bob withdrew $1.
+        BobsBalance -= 1;
+
+        // Bob deposited his paycheck that doubled his current balance.
+        BobsBalance *= 2;
+
+        //Bob deposited $1.
+        BobsBalance += 1;
+
+        System.out.println("Bob's new balance is: $" + BobsBalance);
+    }
+}
